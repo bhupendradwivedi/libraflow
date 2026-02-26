d90a85f7ce8100e10d820ba9e1c395dbea43afb7
bhupendradwivedi <bhupendradwivedi9752@gmail.com>
2026-02-26 12:57:29 +0530

    frontend second

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -3,7 +3,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:3000/api",
+    baseURL: " https://libraflow-g3k0.onrender.com/api",
     withCredentials: true,
     headers: {
         "Content-Type": "application/json",
