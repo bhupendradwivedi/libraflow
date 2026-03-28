6608deec9cb50b06e911232b5753eca2a2d09cfc
bhupendradwivedi <bhupendradwivedi9752@gmail.com>
2026-03-28 11:36:46 +0530

    new url setup2

--- backend/src/app.js
+++ backend/src/app.js
@@ -18,9 +18,29 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+
+import cors from 'cors';
+
+const allowedOrigins = [
+  'http://localhost:5173',               
+  'https://libraflow-one.vercel.app'     
+];
+
 app.use(cors({
-  origin:"https://libraflow-one.vercel.app",
-  credentials: true
+  origin: function (origin, callback) {
+    // allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    
+    if (allowedOrigins.indexOf(origin) === -1) {
+      var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true, // Cookies aur Auth headers ke liye zaroori hai
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 connectDB();
