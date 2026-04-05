7f57ea03590a54b74dac0383360645e2ea0ef67c
bhupendradwivedi <bhupendradwivedi9752@gmail.com>
2026-04-05 11:27:42 +0530

    token setup

--- backend/src/utils/sendToken.js
+++ backend/src/utils/sendToken.js
@@ -2,15 +2,13 @@ export const sendToken = (user, statusCode, message, res) => {
     const accessToken = user.generateAuthToken(); 
     const refreshToken = user.generateRefreshToken();
 
-    // Cookie settings - Localhost Friendly
     const cookieOptions = {
-        expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), // 7 din
+        expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), // 7 D
         httpOnly: true, 
-        secure: false,   // Localhost par HTTP hota hai, isliye false zaroori hai
-        sameSite: 'lax', // Localhost par 'lax' sabse best kaam karta hai
-        path: "/",       // Taaki cookie poore site par mile
+        path: "/",
+        secure: true,     
+        sameSite: "none"   
     };
-
     res.status(statusCode)
        .cookie("refreshToken", refreshToken, cookieOptions) 
        .json({
